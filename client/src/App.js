ae29d6ba479714655ba2e91c8ea3ecad253aa4e8
Daniel Reed <danieel.reed@gmail.com>
2020-03-26 11:49:41 +0100

    cleaned react, moved server to api, connected react to server with axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const testApi = async () => {
+    const request = await axios.get('/api');
+    console.log(request);
+  };
+
+  useEffect(() => {
+    testApi();
+  });
+
+  return <div className="App">oh hey there</div>;
 }
-
-export default App;
