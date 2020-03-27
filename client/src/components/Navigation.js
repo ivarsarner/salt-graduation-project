766040ab4fa530c40a086ae609af87aa2a9f245f
Daniel Reed <danieel.reed@gmail.com>
2020-03-27 11:12:14 +0100

    added checkout items total

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import logo from '../assets/logo-black.svg';
+import { CheckoutContext } from '../context/CheckoutContext';
 
 export default function Navigation() {
+  const { data } = useContext(CheckoutContext);
+  const [currentStore, setCurrentStore] = useState('');
+
+  useEffect(() => {
+    // const getCurrentStore = data[0];
+    // console.log(getCurrentStore.merchantName || 'jeff');
+  });
+
   return (
     <>
       <img src={logo} alt="Way" width="100px" />
+      <p>Current Store: ICA Supermarket Sabbatsberg</p>
     </>
   );
 }
--- client/src/components/checkout/CheckoutCard.js
+++ client/src/components/checkout/CheckoutCard.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Moment from 'react-moment';
 
 import Header from './Header';
 import Item from './Item';
 
 export default function Checkout({ data, checkoutID }) {
+  const [checkoutTotal, setCheckoutTotal] = useState(0);
+
+  useEffect(() => {
+    const reducer = data.items.reduce(
+      (acc, current) => acc + current.quantity,
+      0
+    );
+    setCheckoutTotal(reducer);
+  });
+
   return (
     <div>
       <h4>Checkout {checkoutID}</h4>
       <p>
-        Time:
-        <Moment fromNow>{data.timeCreated}</Moment>
+        Time: <Moment fromNow>{data.timeCreated}</Moment>
       </p>
-      <p>Store: {data.merchantName}</p>
-      <p>Items</p>
+      <p>{checkoutTotal} items</p>
       <div className="item-container">
         {data.items.map((item) => (
           <Item key={item.gtin} itemData={item} />
