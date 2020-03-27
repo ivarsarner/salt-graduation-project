04c912974f5103d4338344663b415a949ec92d3c
Oskar Näsman <oskar.nasman@hotmail.com>
2020-03-27 13:31:54 +0100

    added user api route

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/api', (req, res) => res.send('Hello World!'));
+
+app.get('/api/users', async (req, res) => {
+  const { data } = await axios.get(
+    'https://randomuser.me/api/?nat=gb&results=10&noinfo&exc=location,login,id,email,dob,phone,cell,registered'
+  );
+  const users = data.results.map((user) => ({
+    name: user.name.first + user.name.last,
+    picture: user.picture.large,
+  }));
+  res.json(users);
+});
 
 const server = app.listen(port);
 
