ab2bf82bfa8f3b039fc11b3c37ec634bf2d13289
Daniel Reed <danieel.reed@gmail.com>
2020-03-27 10:29:56 +0100

    fixed test demo

--- client/src/__tests__/Navigation.test.js
+++ client/src/__tests__/Navigation.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../App';
+import Navigation from '../components/Navigation';
 
 test('renders learn react link', () => {
-  const { getByAltText } = render(<App />);
+  const { getByAltText } = render(<Navigation />);
   const wayLogo = getByAltText(/way/i);
   expect(wayLogo).toBeInTheDocument();
 });
