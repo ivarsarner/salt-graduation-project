e980764b84ad4a240ff5bd12cabd18884e031b09
Daniel Reed <danieel.reed@gmail.com>
2020-03-28 20:24:15 +0100

    demo for react enzyme testing with context

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post('*', (_, res) => res.status(400).end());
 app.delete('*', (_, res) => res.status(400).end());
 app.patch('*', (_, res) => res.status(400).end());
 
-app.get('/api', (req, res) => res.send('Way Merchant API'));
+app.get('/api', (_, res) => res.send('Way Merchant API'));
 
 app.use('/api/customers', customersRoutes);
 app.use('/api/products', productsRoutes);
