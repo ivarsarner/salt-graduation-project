2e8f90a2351b62b62dd295a62808e21b3fbeb325
Oskar Näsman <oskar.nasman@hotmail.com>
2020-03-29 16:42:52 +0200

    Fixed button which mocks a new checkout and did some styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState, useEffect } from 'react';
 import logo from '../assets/logo-black.svg';
 import { CheckoutContext } from '../context/CheckoutContext';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
-  const { data } = useContext(CheckoutContext);
+  const { data, actions } = useContext(CheckoutContext);
   const [currentStore, setCurrentStore] = useState(null);
 
   useEffect(() => {
@@ -15,9 +16,20 @@ export default function Navigation() {
   }, [data]);
 
   return (
-    <>
-      <img src={logo} alt="Way" width="100px" />
-      <p>Current Store: {currentStore}</p>
-    </>
+    <div className="main-nav">
+      <NavLink className="main-nav-logo" exact to="/">
+        <img src={logo} alt="Way" width={100} />
+      </NavLink>
+      <p>{currentStore}</p>
+      <div className="main-nav-links">
+        <NavLink to="/other">Other</NavLink>
+        <button
+          className="new-checkout-button"
+          onClick={() => actions.addNewCheckout()}
+        >
+          Add new Checkout
+        </button>
+      </div>
+    </div>
   );
 }
--- client/src/context/CheckoutContext.js
+++ client/src/context/CheckoutContext.js
@@ -1,20 +1,39 @@
 import React, { createContext, useState, useEffect } from 'react';
 import mockData from '../assets/order-mock-data.json';
+import newMockData from '../assets/new-checkouts-mock-data.json';
+import moment from 'moment';
 
 export const CheckoutContext = createContext();
 
 const CheckoutContextProvider = (props) => {
   const [data, setData] = useState([]);
+  const [newCheckoutData, setNewCheckoutData] = useState([]);
 
   useEffect(() => {
-    const filterData = mockData.filter(
-      (checkout) => checkout.merchant === 'IfO0fugaM9XRaaICJ7LQ'
-    );
+    const filterData = mockData
+      .filter((checkout) => checkout.merchant === 'IfO0fugaM9XRaaICJ7LQ')
+      .map((checkout) => ({
+        ...checkout,
+        checkout: Math.floor(Math.random() * 4) + 1,
+      }));
+    setNewCheckoutData(newMockData);
     setData(filterData);
   }, []);
 
+  const addNewCheckout = () => {
+    let newCheckout = newCheckoutData.splice(-1, 1);
+    newCheckout = {
+      ...newCheckout[0],
+      timeCreated: moment().format('YYYY-MM-DDTHH:mm:ss'),
+      checkout: Math.floor(Math.random() * 4) + 1,
+    };
+    setData((prevState) => {
+      return [...prevState, newCheckout];
+    });
+  };
+
   return (
-    <CheckoutContext.Provider value={{ data }}>
+    <CheckoutContext.Provider value={{ data, actions: { addNewCheckout } }}>
       {props.children}
     </CheckoutContext.Provider>
   );
