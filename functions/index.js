98a2ad69d9b47c0ad4a4856d1272bd2b2a425af8
Daniel Reed <danieel.reed@gmail.com>
2020-04-02 17:06:06 +0200

    done but we can't use this with google unless we have a paid account

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,17 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 
 const app = express();
 const { customersRoutes, productsRoutes } = require('./routes');
 
-app.use(cors({ origin: true }));
+app.use(cors());
+app.use(
+  compression({
+    level: 7,
+  })
+);
 
 app.post('*', (_, res) => res.sendStatus(400));
 app.delete('*', (_, res) => res.sendStatus(400));
