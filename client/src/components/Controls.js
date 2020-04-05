4cc6b8f7f5cf727644aeb7da25cfb6f37d12e663
Isabelle-123 <isacas031@gmail.com>
2020-04-05 23:23:04 +0200

    changed button + navbar in mobile view

--- client/src/components/Controls.js
+++ client/src/components/Controls.js
@@ -7,8 +7,29 @@ const ControlsDiv = styled.div`
   @media (max-width: 625px) {
     display: flex;
     margin: 10px auto;
-    button:first-child {
+    button {
       margin-right: 10px;
+      color: #202129;
+      background-color: #ffffff;
+      box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2);
+      height: 30px;
+      width: 70px;
+      display: flex;
+      text-align: center;
+      margin: 5px;
+      padding: 0px;
+      font-size: 0.7rem;
+
+      &:hover {
+        color: #202129;
+        background-color: #e1e2e2;
+        opacity: 1;
+      }
+
+      &:active {
+        background-color: #d5d6d6;
+        opacity: 1;
+      }
     }
   }
 `;
